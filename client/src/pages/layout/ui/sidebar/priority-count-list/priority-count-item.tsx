64f7a44b374819ea302eef2b1cf66b5080338bcb
loxxyN <emoyko@yandex.ru>
2026-08-25 05:28:36 +0700

    style(layout): Уменьшил ширину сайдбара refactor(priority-count-item): Приоритет отображается в виде круга до label, count затемнил

--- client/src/pages/layout/ui/sidebar/priority-count-list/priority-count-item.tsx
+++ client/src/pages/layout/ui/sidebar/priority-count-list/priority-count-item.tsx
@@ -1,26 +1,25 @@
-import { Chip } from '@heroui/react'
 import type { TodoPriority } from '@shared/types'
 
-type PriorityStatus = 'success' | 'warning' | 'danger'
 interface PriorityCountItem {
   label: string
   count: number
   priority: TodoPriority
 }
 
-const priorityMap: Record<TodoPriority, PriorityStatus> = {
-  low: 'success',
-  medium: 'warning',
-  high: 'danger',
+const colorMap = {
+  high: 'bg-danger',
+  medium: 'bg-warning',
+  low: 'bg-success',
 }
 
 export const PriorityCountItem = ({ label, count, priority }: PriorityCountItem) => {
   return (
     <div className="py-2 flex items-center justify-between">
-      <p className="text-sm">{label}</p>
-      <Chip className="min-w-5" variant="primary" size="sm" color={priorityMap[priority]}>
-        <span className="w-full flex justify-center">{count}</span>
-      </Chip>
+      <div className="flex items-center gap-3">
+        <span className={`size-2 rounded-full ${colorMap[priority]}`}></span>
+        <p className="text-sm">{label}</p>
+      </div>
+      <span className="text-muted text-xs">{count}</span>
     </div>
   )
 }
